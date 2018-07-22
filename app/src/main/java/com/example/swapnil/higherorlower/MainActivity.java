3e4867527a04d51b593f190d2e29ae33113ae920
swapnil <swapnilsinha20@gmail.com>
2018-07-22 18:51:04 +0530

    Changed Text and Welcome Toast added

--- app/src/main/java/com/example/swapnil/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/swapnil/higherorlower/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.swapnil.higherorlower;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -11,36 +11,39 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     int randomNumber;
-    public void generateRandomNumber(){
-        Random random= new Random();
-        randomNumber=random.nextInt(20)+1;
+
+    public void generateRandomNumber() {
+        Random random = new Random();
+        randomNumber = random.nextInt(20) + 1;
 
     }
-    public void guess(View view){
-        Log.i("info","button pressed");
-        EditText editText=(EditText)findViewById(R.id.editText);
+
+    public void guess(View view) {
+        Log.i("info", "button pressed");
+        EditText editText = (EditText) findViewById(R.id.editText);
         String str = editText.getText().toString();
-        if (str.isEmpty()){
+        if (str.isEmpty()) {
             Toast.makeText(this, "Please Enter Some Value", Toast.LENGTH_SHORT).show();
+        } else {
+            int guess = Integer.parseInt(str);
+            String message;
+            if (guess > randomNumber) {
+                message = "lower!";
+            } else if (guess < randomNumber) {
+                message = "higher!";
+            } else {
+                message = "You Got It Right! You can Play Again";
+                generateRandomNumber();
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         }
-        else {
-        int guess =Integer.parseInt(str);
-        String message;
-        if (guess > randomNumber){
-            message ="lower!";
-        }
-        else if (guess < randomNumber){
-            message ="higher!";
-        }else{
-            message ="You Got It Right! You can Play Again";
-            generateRandomNumber();
-        }
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }}
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         generateRandomNumber();
+        Toast.makeText(this, "Welcome to Higher or Lower Game", Toast.LENGTH_LONG).show();
     }
 }
